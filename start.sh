3060790757f8be9ff9fc08f92f73ff108efaf629
anmol0700k <117492721+anmol0700k@users.noreply.github.com>
2022-11-07 10:09:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Anmolkanchru/Evamaria.git /EvaMaria
+  git clone https://github.com/anmol0700k/Evamaria-jhonny-2.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
